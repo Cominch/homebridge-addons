17a370c1ec970414e8e0d593e3fe7036aedce5a4
Cominch <dominic@luetjohann.com>
2017-07-02 14:34:29 +0000

    initial commit

---
+++ install-homebridge-addons.sh
@@ -0,0 +1,8 @@
+
+npm install -g homebridge-samsungtv-control
+
+npm install -g homebridge-fritzbox
+
+cp homebridge-scripts/homebridge-config.json ~/.homebridge/config.json
+
+systemctl restart homebridge.service
